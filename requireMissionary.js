d1740d80f474dc21a362181dff87a123d997153b
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-01-17 08:23:15 +0100

    Update requireMissionary.js

--- requireMissionary.js
+++ requireMissionary.js
@@ -12,14 +12,11 @@ document.write('<script src="https://accounts.google.com/gsi/client" async defer
 function handleGoogleLogin(response) {
     console.log(response);
     const responsePayload = jwt_decode(response.credential);
-
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
-    alert("Email: " + responsePayload.email);
+    if (responsePayload.email.split('@')[1].toLowerCase().includes('missionary.org') || responsePayload.email.split('@')[1].toLowerCase().includes('churchofjesuschrist.org')) {
+        alert('good');
+    } else {
+        alert('no-good');
+    }
 }
 window.onload = function () {
     const gBtn = document.createElement('DIV');
