898555d4469234a5e3f9777c35cc7fdd67b4f764
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-01-17 20:26:10 +0100

    added cookie functions

--- requireMissionary.js
+++ requireMissionary.js
@@ -1,3 +1,37 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
 const pageDivId = document.currentScript.getAttribute('containerDIV') || null;
 if (pageDivId == null) {
     throw new Error('You must specify the page container to be hidden if not logged in. For instance: <script src="requireMissionary.js" containerDiv="mainPage"></script>');
@@ -17,7 +51,7 @@ html, body, #ssmLogoBIG_forSignIn {
 }
 </style>` + document.head.innerHTML;
 
-
+//https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js
 
 function handleGoogleLogin(response) {
     console.log(response);
