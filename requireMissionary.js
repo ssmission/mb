7ac0f13b08611efc3f9ad3e42775e3de0f37b449
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-01-17 20:51:18 +0100

    made expiration optional

--- requireMissionary.js
+++ requireMissionary.js
@@ -1,8 +1,11 @@
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(cname, cvalue, exdays='') {
+    let expires = '';
+    if (exdays != '') {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        expires = ";expires="+d.toUTCString();
+    }
+    document.cookie = cname + "=" + cvalue + expires + ";path=/";
 }
 
 function getCookie(cname) {
