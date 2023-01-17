6feb94a1b336c1240afa9e92e9e2f8bad4707d3b
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-01-17 20:47:17 +0100

    created cookies for site

--- requireMissionary.js
+++ requireMissionary.js
@@ -20,29 +20,40 @@ function getCookie(cname) {
     return "";
 }
 
+let ssmUserStr = getCookie('ssmUser');
+let ssmUser = {};
+let mustLogIn = true;
+if (ssmUserStr != '') {
+    ssmUser = JSON.parse(ssmUserStr);
+    if (ssmUser.email.toLowerCase().includes('@missionary.org')) {
+        mustLogIn = false;
+    }
+}
+
 const pageDivId = document.currentScript.getAttribute('containerDIV') || null;
 if (pageDivId == null) {
     throw new Error('You must specify the page container to be hidden if not logged in. For instance: <script src="requireMissionary.js" containerDiv="mainPage"></script>');
 }
 const pageDIV = document.getElementById(pageDivId).cloneNode(true);
-document.getElementById(pageDivId).remove();
+if (mustLogIn) {
+    document.getElementById(pageDivId).remove();
 
-document.write(`<script src="https://accounts.google.com/gsi/client" async defer>
-</script><script src="https://unpkg.com/jwt-decode/build/jwt-decode.js"></script>
-<img id="ssmLogoBIG_forSignIn" src="img/ssmLogo.png" alt="ssmLogo">`);
+    document.write(`<script src="https://accounts.google.com/gsi/client" async defer>
+    </script><script src="https://unpkg.com/jwt-decode/build/jwt-decode.js"></script>
+    <img id="ssmLogoBIG_forSignIn" src="img/ssmLogo.png" alt="ssmLogo">`);
 
-document.head.innerHTML = `<style id="signInSTYLES">
-html, body, #ssmLogoBIG_forSignIn {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
+    document.head.innerHTML = `<style id="signInSTYLES">
+    html, body, #ssmLogoBIG_forSignIn {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+    </style>` + document.head.innerHTML;
 }
-</style>` + document.head.innerHTML;
 
-//https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js
+
 
 function handleGoogleLogin(response) {
-    console.log(response);
     const responsePayload = jwt_decode(response.credential);
     if (responsePayload.email.split('@')[1].toLowerCase().includes('missionary.org') || responsePayload.email.split('@')[1].toLowerCase().includes('churchofjesuschrist.org')) {
         document.body.prepend(pageDIV);
@@ -50,13 +61,23 @@ function handleGoogleLogin(response) {
         document.getElementById('google_btn').remove();
         document.getElementById('signInSTYLES').remove();
         document.getElementById('ssmLogoBIG_forSignIn').remove();
+
         // set images and stuff like that to see that their logged in
+        ssmUser = {
+            'id' : responsePayload.sub,
+            'full_name' : responsePayload.name,
+            'given_name' : responsePayload.given_name,
+            'family_name' : responsePayload.family_name,
+            'picture' : responsePayload.picture,
+            'email' : responsePayload.email,
+        };
+        setCookie('ssmUser', JSON.stringify(ssmUser));
     } else {
         // clear login cookies
         document.write('<h2>Access Denied</h2><br><br>Sorry, you don\'t have access to this page because you\'re not a missionary. If you beleive this is a mistake, try <a href=".">logging in again with your missionary account</a>');
     }
 }
-window.onload = function () {
+function startGoogleSignIn() {
     const gBtn = document.createElement('DIV');
     gBtn.id = 'google_btn';
     document.body.prepend(gBtn);
@@ -81,4 +102,7 @@ window.onload = function () {
             google.accounts.id.prompt();
         }
     });
+}
+if (mustLogIn) {
+    window.onload = startGoogleSignIn;
 }
