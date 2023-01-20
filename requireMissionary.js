40048dd07cf22a1aa03a54e8007cb2c28d6832b7
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-01-20 16:23:33 +0100

    added ux_mode: "redirect"

--- requireMissionary.js
+++ requireMissionary.js
@@ -140,6 +140,7 @@ function startGoogleSignIn() {
     if(true) { //isFacebookBrowser()
         google.accounts.id.initialize({
             client_id: '1032123159845-v6r228c1sb4su1gdck40a2kctu8k8riu.apps.googleusercontent.com',
+            ux_mode: "redirect",
             login_uri: 'https://www.httpbin.org/post',
             cancel_on_tap_outside: false
         });
